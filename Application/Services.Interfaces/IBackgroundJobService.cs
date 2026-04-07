e31db2c2dd37c153d7710609c48750df456c6eb7
TempName460 <asuslaptopacc7@gmail.com>
2026-04-07 15:59:53 +0600

    feat(application): add background job, email, and notification service abstractions

---
+++ Application/Services.Interfaces/IBackgroundJobService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace Application.Services.Interfaces;
+
+public interface IBackgroundJobService
+{
+    void Enqueue<T>(Expression<Func<T, Task>> job);
+
+    void Schedule<T>(Expression<Func<T, Task>> job, TimeSpan delay);
+}
